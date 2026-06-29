accec2b3595a50931090a297444997c5136f88ad
wifimoney <elibelilty1998@gmail.com>
2026-06-29 17:11:48 +0100

    appName + unreademail and logged in added

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+const appName: string = "TS Email Reader";
+const unreadEmails: number = 12; 
+const isLoggedIn: boolean = true;
+
+console.log(appName);
+console.log(unreadEmails);
+console.log(isLoggedIn);
